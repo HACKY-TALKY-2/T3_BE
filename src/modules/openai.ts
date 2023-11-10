1ff44eca2a8964a16c9b0a110a4a5c52ec5f1b87
withsang <jsa5115@naver.com>
2023-11-11 02:01:10 +0900

    feat(openai): gpt-4 turbo

--- src/modules/openai.ts
+++ src/modules/openai.ts
@@ -9,7 +9,8 @@ export const getReply = async (prompt: string = "Please say hello to me.") => {
   try {
     const params: OpenAI.Chat.ChatCompletionCreateParams = {
       messages: [{ role: "user", content: prompt }],
-      model: "gpt-3.5-turbo",
+      model: "gpt-4-1106-preview", // GPT-4 Turbo!
+      temperature: parseFloat(process.env.CHAT_GPT_TEMPERATURE || "0.5"),
     };
     const chatCompletion: OpenAI.Chat.ChatCompletion =
       await openai.chat.completions.create(params);
